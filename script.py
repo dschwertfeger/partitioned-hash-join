2101d2b7c1ad6aef61d275ca009555aa295e0954
David Schwertfeger <david.schwertfeger@googlemail.com>
2014-11-17 18:10:22 +0100

    Avoid string manipulation operations and use xrange instead of range.

--- script.py
+++ script.py
@@ -6,47 +6,37 @@ R = 'r10m.txt'  # 'file1.txt'
 S = 's10m.txt'  # 'file2.txt'
 
 def init_buckets(name):
-    return [open('{}_{}.txt'.format(name, i), 'w') for i in range(NR_OF_BUCKETS)]
+    return [open('{}_{}.txt'.format(name, i), 'w') for i in xrange(NR_OF_BUCKETS)]
 
 def h1(line):
     return int(line[1:3]) % NR_OF_BUCKETS
-
-def make_row(line):
-    row = [line[1:10]]
-    row.append(line[:1])
-    str = ','.join(row)
-    return str + '\n'
-
-def get_size(file):
-    file.seek(0, 2)
-    return file.tell()
 
 def build_hash_table(from_file):
     hash_table = dict()
     with open(from_file.name, 'r') as f:
         for line in f:
-            key, value = line.split(',')
-            value = value[:1]
+            key = line[1:11]
             # elimitinate duplicates
             if key in hash_table:
-                hash_table[key].add(value)
+                hash_table[key].add(line)
             else:
-                hash_table[key] = set(value)
+                hash_table[key] = set()
+                hash_table[key].add(line)
     return hash_table
 
 def partition(src_file, to):
     with open(src_file, 'rt') as f:
         for line in f:
             bucket = h1(line)
-            row = make_row(line)
-            to[bucket].write(row)
+            to[bucket].write(line)
 
 def join_buckets():
     result = dict()
-    for bucket in range(NR_OF_BUCKETS):
+    for bucket in xrange(NR_OF_BUCKETS):
         hash_table = build_hash_table(r[bucket])
         part = join(hash_table, s[bucket])
         result.update(part)
+        del part
         del hash_table
     return result
 
@@ -54,18 +44,17 @@ def join(hash_table, file):
     results = dict()
     with open(file.name, 'r') as f:
         for line in f:
-            key, value = line.split(',')
-            value = value[:1]
+            key = line[1:11]
             if key in hash_table:
                 results[key] = hash_table[key]
-                results[key].add(value)
+                results[key].add(line)
     return results
 
 def write(result_dict):
     with open('result.txt', 'w') as f:
-        for key, values in result_dict.iteritems():
-            for v in values:
-                f.write(u'{}{}\n'.format(v, key))
+        for k, value in result_dict.iteritems():
+            for v in value:
+                f.write(v)
 
 
 if __name__=='__main__':
